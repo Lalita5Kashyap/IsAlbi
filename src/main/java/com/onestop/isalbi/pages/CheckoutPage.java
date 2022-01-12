62bfe1f80c717ae2280b0a3df90bef6ff054e3e3
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2022-01-12 12:06:02 +0530

    Push Checkout page class changes

--- src/main/java/com/onestop/isalbi/pages/CheckoutPage.java
+++ src/main/java/com/onestop/isalbi/pages/CheckoutPage.java
@@ -19,14 +19,17 @@ public class CheckoutPage extends Multimaplibraries {
 	
 	public static void FillCCDetails(String TestCase) {
 		getTestData(IsAlbiConstants.TestData, className);
-		BrowserActions.isFrameSwitch(CheckoutPageObjects.frameswitch);
-		BrowserActions.isdefaultFrameSwitch();
 		final String CardNumber = getTestDataCellValue(TestCase, "Card Number");
 		System.out.println(CardNumber);
 		final String ExpiryDate = getTestDataCellValue(TestCase, "Expiry Date");
 		System.out.println(ExpiryDate);
 		final String cvc = getTestDataCellValue(TestCase, "CVC");
 		System.out.println(cvc);
+		BrowserActions.isSleep();
+		BrowserActions.isFrameSwitch(CheckoutPageObjects.frameswitch);
+		BrowserActions.isSleep();
+		//BrowserActions.isdefaultFrameSwitch();
+		
 		BrowserActions.isSetValue(CheckoutPageObjects.cardnumber, CardNumber);
 		BrowserActions.isSetValue(CheckoutPageObjects.expirydate, ExpiryDate);
 		BrowserActions.isSetValue(CheckoutPageObjects.cvc, cvc);
